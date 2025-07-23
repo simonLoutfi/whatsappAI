5ce02ca78a71b0050435d3295af3be8484cc9459
simonLoutfi <simon.loutfi@hotmail.com>
2025-07-23 15:43:22 +0300

    Update whatsappHandler.js

--- test/whatsappHandler.js
+++ test/whatsappHandler.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const TOKEN = 'EAAULrxtQZB0oBPOj8SIkwJtjxGu9fyfaDCdYhBhMxNsHLbmFTlUMpzvpjLI1ciQI67xLP5kMiWQMTQmRZBTpeJgN365bMw3BXTy4RKxbE4W80yZA2GmCoZAMNaN31eowB5pWA34qVYgcwTOhmQVMcdw8gqiqaeB3lkqckxTPAEHlwsE50U5arqfTV5GZCkJnV3l9uNHIkNFV2foZBQmnE1dxak1queTU8zXSkb0yboYDLcpAZDZD';
-const PHONE_NUMBER_ID = '1290492105934901';
+const PHONE_NUMBER_ID = '658554300685107';
 
 async function sendWhatsAppMessage(to, message) {
   const url = `https://graph.facebook.com/v23.0/${PHONE_NUMBER_ID}/messages`;
