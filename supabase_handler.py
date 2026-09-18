47da5b2a43e7395addc52942167a19041bddd87d
Simon Loutfi <148797285+simonLoutfi@users.noreply.github.com>
2026-09-18 12:53:38 +0300

    Update supabase_handler.py

--- supabase_handler.py
+++ supabase_handler.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 
-url = "https://qvijckgxoauxeyoowcsn.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2aWpja2d4b2F1eGV5b293Y3NuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Mjc0MjUxOSwiZXhwIjoyMDY4MzE4NTE5fQ.iVbqPo0-YdzSwyPD0ZNfCUqbM31lm9tlH-9IihePo98"
+url = ""
+key = ""
 supabase = create_client(url, key)
 
 def get_faq_stock_and_profile():
